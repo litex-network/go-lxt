2c7b744d402835cfe3f704744c932251091c6e46
litex-network <87160769+litex-network@users.noreply.github.com>
2022-01-27 11:38:47 +0800

    avoid using blockCtx concurrently

--- eth/tracers/tracers_test.go
+++ eth/tracers/tracers_test.go
@@ -120,6 +120,7 @@ type callTracerTest struct {
 	Result  *callTrace    `json:"result"`
 }
 
+
 func TestPrestateTracerCreate2(t *testing.T) {
 	unsignedTx := types.NewTransaction(1, common.HexToAddress("0x00000000000000000000000000000000deadbeef"),
 		new(big.Int), 5000000, big.NewInt(1), []byte{})
